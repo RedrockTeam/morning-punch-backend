d11f538c6b83ed8bc181c5f63cae0dce43ffd9a2
coderming <coderming@foxmail.com>
2018-12-19 13:22:18 +0800

    feat: 添加punch非法参数校验

--- routes/punch.ts
+++ routes/punch.ts
@@ -1,7 +1,7 @@
 import * as KoaRouter from 'koa-router'
 
 const router = new KoaRouter()
-import { getOne, setOne } from '../lib/mysql'
+import { getOne } from '../lib/mysql'
 import { RouterFormat } from './index'
 
 router.get('/', async ctx => {
@@ -12,7 +12,14 @@ router.get('/', async ctx => {
     ctx.body.status = 0
     ctx.body.errmsg = '该openid尚未登记时间'
   } else {
-    const { openid, punch_time, score } = userData
+    const { openid, punch_time } = userData
+    let prevInfo = <{ [key: string]: any }>await getOne(openid)
+    // 处理非法参数
+    if (prevInfo.punch_time !== punch_time) {
+      ctx.body.status = 0
+      ctx.body.errmsg = `传入的签到时间与之前设定的签到时间不符合`
+      return
+    }
   }
 })
 
