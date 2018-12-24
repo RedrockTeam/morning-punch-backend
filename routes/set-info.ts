2bc21ad8a315abb8ee5cfdd8d44168f70b4ce0ae
coderming <coderming@foxmail.com>
2018-12-24 21:19:39 +0800

    style: 删log

--- routes/set-info.ts
+++ routes/set-info.ts
@@ -5,9 +5,7 @@ import { getOne, setOne } from '../lib/mysql'
 import { RouterFormat } from './index'
 
 router.post('/', async ctx => {
-  console.log(1231)
   const { openid, punch_time } = <{ [key: string]: any }>ctx.request.body
-  console.log(<{ [key: string]: any }>ctx.request.body)
   if ((await getOne(openid)).openid) {
     ctx.body.status = 0
     ctx.body.errmsg = '已经有该openid了'
